b70b5aa4974f9dc6d6877a545f3e8b9dd808ff9c
AC-climoilou <1701633@cegeplimoilou.ca>
2023-05-08 20:11:33 -0400

    Ma version

---
+++ TP3AppWeb/TP3AppWeb/Models/Evaluation.cs
@@ -0,0 +1,26 @@
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TP3AppWeb.Models
+{
+    public class Evaluation
+    {
+        // Enumeration
+        public enum CoteDeJeu
+        {
+            EXCELLENT = 5,
+            TRES_BON = 4,
+            BON = 3,
+            MOYEN = 2,
+            MAUVAIS = 1
+        }
+
+        // Attributs
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int EvaluationID { get; set; }
+
+        public CoteDeJeu Cote { get; set; }
+        public string Description { get; set; }
+
+    }
+}
